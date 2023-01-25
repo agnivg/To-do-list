575b1688fa9628a51c4224fa48b30b911bc9b7ec
AmazingAryan <92223455+AmazingAryan@users.noreply.github.com>
2023-01-25 23:20:20 +0530

    Changes in Navbar (#9)
    * Changes in Navbar
    
    * Updates in navbar
    
    * Nav changes
    
    * some corrections for navbar
    
    * updated color scheme of navbar

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import './index.css';
 
-ReactDOM.render(
-  <React.Fragment>
-    <App />    
-  </React.Fragment>,
-  document.getElementById('root')
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <App />
 );
