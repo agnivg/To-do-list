61f143a2af313ec061d781b31b5357e2fdb6f439
pranav madhu <pranavmadhu73@gmail.com>
2023-01-26 18:05:23 +0530

    removed double import of Navbar in app component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import LoginSignup from './pages/user/LoginSignup/LoginSignup';
 import Navbar from './components/Navbar/Navbar'
-import Navbar from './components/Navbar/Navbar'
 import './App.css'
 import Dashboard from './pages/user/Dashboard/Dashboard'
 
