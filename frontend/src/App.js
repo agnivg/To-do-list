81d02aefbc92acb093d02186ff9238dcea3fcc75
pranav madhu <pranavmadhu73@gmail.com>
2023-01-26 17:37:20 +0530

    Added navbar in Login Signup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import LoginSignup from './pages/user/LoginSignup/LoginSignup';
+import Navbar from './components/Navbar/Navbar'
 import './App.css'
 
 function App() {
@@ -7,7 +8,7 @@ function App() {
   return (
     <>
       <div className="App">
-      {/* <Navbar/> */}
+      <Navbar/>
       <LoginSignup />
       </div>  
     </>
