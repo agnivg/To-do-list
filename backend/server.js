259f3528978fc7ddb183fa0b7f271d8a75e5a1b3
pranav madhu <pranavmadhu73@gmail.com>
2023-03-11 08:23:57 +0530

    some configuration changes

--- backend/server.js
+++ backend/server.js
@@ -1,69 +1,69 @@
-const dotenv = require('dotenv')
-const express = require('express')
-const cors = require('cors')
-const passport = require('passport')
-const session = require('express-session')
-const MongoDBStore = require('connect-mongodb-session')(session)
+const dotenv = require("dotenv");
+const express = require("express");
+const cors = require("cors");
+const passport = require("passport");
+const session = require("express-session");
+const MongoDBStore = require("connect-mongodb-session")(session);
 
-const connectDB = require('./config/db')
+const connectDB = require("./config/db");
 
-const app = express()
+const app = express();
 
 // Load env vars
-dotenv.config({ path: './env/.env' })
+// dotenv.config({ path: './env/.env' })
+dotenv.config();
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 1337;
 
 // DB Connection
-connectDB()
+connectDB();
 
 // Route Imports
 const {
-    AuthRoutes,
-    TaskRoutes,
-    SubtaskRoutes,
-    BadgeRoutes,
-} = require('./routes')
-
+  AuthRoutes,
+  TaskRoutes,
+  SubtaskRoutes,
+  BadgeRoutes,
+} = require("./routes");
 
 // Middleware
-app.use(express.json({ extended: false }))
-app.use(express.urlencoded({ extended: false }))
-app.use(cors({ origin: true, credentials: true }))
+app.use(express.json({ extended: false }));
+app.use(express.urlencoded({ extended: false }));
+app.use(cors({ origin: true, credentials: true }));
 app.use(
-    session({
-        secret: 'jhdlskajdkljaslkdjlkasjdalksjddlkjasdlkjasdkjalwkdjalksjdlkajsd',
-        resave: false,
-        saveUninitialized: false,
-        store: new MongoDBStore({
-            uri: process.env.MONGO_URI,
-            collection: 'sessions',
-        }),
-    })
-)
-app.use(passport.initialize())
-app.use(passport.session())
+  session({
+    secret: "jhdlskajdkljaslkdjlkasjdalksjddlkjasdlkjasdkjalwkdjalksjdlkajsd",
+    resave: false,
+    saveUninitialized: false,
+    store: new MongoDBStore({
+      uri: process.env.MONGO_URI,
+      collection: "sessions",
+    }),
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
 // Serialize and Deserialize User
 passport.serializeUser(function (user, cb) {
-    cb(null, user)
-})
+  cb(null, user);
+});
 
 passport.deserializeUser(function (user, cb) {
-    process.nextTick(function () {
-        return cb(null, user)
-    })
-})
+  process.nextTick(function () {
+    return cb(null, user);
+  });
+});
 
 // Routes
-app.get('/', (req, res) => res.send('Server up and running'))
+app.get("/", (req, res) => res.send("Server up and running"));
 
 // Route middlewares
-app.use('/api/auth', AuthRoutes)
-app.use('/api/task', TaskRoutes)
-app.use('/api/subtask', SubtaskRoutes)
-app.use('/api/badge', BadgeRoutes)
+app.use("/api/auth", AuthRoutes);
+app.use("/api/task", TaskRoutes);
+app.use("/api/subtask", SubtaskRoutes);
+app.use("/api/badge", BadgeRoutes);
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`)
-})
+  console.log(`Server is running on port ${PORT}`);
+});
